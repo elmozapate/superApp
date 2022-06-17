9458602573cb7bc92a11ebddeb18446216e48579
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-16 19:05:14 -0500

    moet calamatimesssaas

--- components/containers/relojApp.jsx
+++ components/containers/relojApp.jsx
@@ -4,6 +4,15 @@ const RelojApp = ({ eltiempo = 0}) => {
     return (
         <div className='reloj-puente'>
                 <span>{parseInt(eltiempo / 60)<10?`0${parseInt(eltiempo / 60)}`:parseInt(eltiempo / 60)}:{((eltiempo)-((parseInt(eltiempo / 60))*60))<10?`0${((eltiempo)-((parseInt(eltiempo / 60))*60))}`:((eltiempo)-((parseInt(eltiempo / 60))*60))} </span>
+                {eltiempo > 60 ? <><audio  className="hide" controls loop autoPlay>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fhousehold045.mp3?alt=media&token=defd2868-7e64-4280-8dd9-1339212302bb" type="audio/mp3" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fhousehold045.mp3?alt=media&token=defd2868-7e64-4280-8dd9-1339212302bb" type="audio/mp3" />
+                </audio></> : <><audio className="hide" controls loop autoPlay>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fmisc198.mp3?alt=media&token=3be8cd3f-9b18-4479-b0bc-d828c8197b21" type="audio/mp3" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fmisc198.mp3?alt=media&token=3be8cd3f-9b18-4479-b0bc-d828c8197b21" type="audio/mp3" />
+                </audio></>
+
+                }
             </div>
 
    )
--- pages/paginas/relojPuente.js
+++ pages/paginas/relojPuente.js
@@ -39,15 +39,7 @@ export default function RelojPuente() {
             </div>
             </> : <>
                 <RelojApp eltiempo={eltiempo} />
-                {eltiempo > 60 ? <><audio controls loop autoPlay>
-                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fhousehold045.mp3?alt=media&token=defd2868-7e64-4280-8dd9-1339212302bb" type="audio/mp3" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fhousehold045.mp3?alt=media&token=defd2868-7e64-4280-8dd9-1339212302bb" type="audio/mp3" />
-                </audio></> : <><audio controls loop autoPlay>
-                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fmisc198.mp3?alt=media&token=3be8cd3f-9b18-4479-b0bc-d828c8197b21" type="audio/mp3" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fmisc198.mp3?alt=media&token=3be8cd3f-9b18-4479-b0bc-d828c8197b21" type="audio/mp3" />
-                </audio></>
-
-                }
+               
             </>
         }
     </>)
