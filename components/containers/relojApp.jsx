2c5f66f0e6262a5a1c63fdff2080b397752531a5
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-16 21:31:49 -0500

    moet calamatimesssaasasmusicaasadaas

--- components/containers/relojApp.jsx
+++ components/containers/relojApp.jsx
@@ -32,7 +32,12 @@ const RelojApp = ({ jail = false, winning = false, eltiempo = 0 }) => {
 
     return (
         <div className='reloj-puente'>
-            {
+            {winning ? <>
+                <span>PASS</span> <audio className='hide' controls autoPlay>
+                    <source src={'https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Ffanfare-triumphal.mp3?alt=media&token=86ec38ea-bf88-4656-96aa-148b6ebd7812'
+                    } type="audio/mp3" />
+                </audio>
+            </> :
                 eltiempo > 0 ? <span>{parseInt(eltiempo / 60) < 10 ? `0${parseInt(eltiempo / 60)}` : parseInt(eltiempo / 60)}:{((eltiempo) - ((parseInt(eltiempo / 60)) * 60)) < 10 ? `0${((eltiempo) - ((parseInt(eltiempo / 60)) * 60))}` : ((eltiempo) - ((parseInt(eltiempo / 60)) * 60))} </span>
                     : <><span>FAIL</span>
                         <audio className='hide' controls autoPlay>
