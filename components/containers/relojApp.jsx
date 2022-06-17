21bc61362c3588c09d7574963f2f2f4776451400
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-16 20:44:15 -0500

    moet calamatimesssaasasmusicaasad

--- components/containers/relojApp.jsx
+++ components/containers/relojApp.jsx
@@ -1,13 +1,37 @@
 import { useEffect, useState } from "react"
 let ending = false
-const RelojApp = ({ relojSrc='',winning = false, eltiempo = 0 }) => {
+const RelojApp = ({ winning = false, eltiempo = 0 }) => {
+    const [relojSrc, setrelojSrc] = useState('https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fy2mate.com%20-%20Reloj%20Minutero%20%20Efecto%20de%20Sonidolento.mp3?alt=media&token=aa3d406f-6724-4f87-9715-475f865f282e')
+    useEffect(() => {
+        if (eltiempo) {
+            if (eltiempo < 60 && !ending) {
+                console.log('eoooo');
+                setrelojSrc("https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fy2mate.com%20-%20sonido%20de%20reloj%20tic%20tic.mp3?alt=media&token=b870b388-39e0-4cd7-9e52-cac388629217")
+                ending = true
+            }
+        }
+        console.log('uyyyuy', eltiempo);
+    }, [eltiempo])
+    useEffect(() => {
+        console.log('winnnnnnnnnnnnasn', winning);
+
+        if (winning) {
+            setrelojSrc('https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Ffanfare-triumphal.mp3?alt=media&token=86ec38ea-bf88-4656-96aa-148b6ebd7812')
+            console.log('winnnnnnnnnnnnn', winning);
+
+        }
+    }, [winning])
 
     return (
         <div className='reloj-puente'>
-            <span>{parseInt(eltiempo / 60) < 10 ? `0${parseInt(eltiempo / 60)}` : parseInt(eltiempo / 60)}:{((eltiempo) - ((parseInt(eltiempo / 60)) * 60)) < 10 ? `0${((eltiempo) - ((parseInt(eltiempo / 60)) * 60))}` : ((eltiempo) - ((parseInt(eltiempo / 60)) * 60))} </span>
-            {winning ? <audio className="hide" controls autoPlay>
+            {
+                eltiempo > 0 ? <span>{parseInt(eltiempo / 60) < 10 ? `0${parseInt(eltiempo / 60)}` : parseInt(eltiempo / 60)}:{((eltiempo) - ((parseInt(eltiempo / 60)) * 60)) < 10 ? `0${((eltiempo) - ((parseInt(eltiempo / 60)) * 60))}` : ((eltiempo) - ((parseInt(eltiempo / 60)) * 60))} </span>
+                    : <span>MUERTOS</span>
+
+            }
+            {winning ? <audio className='' controls autoPlay>
                 <source src={'https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Ffanfare-triumphal.mp3?alt=media&token=86ec38ea-bf88-4656-96aa-148b6ebd7812'} type="audio/mp3" />
-            </audio> : <audio className="hide" controls loop autoPlay>
+            </audio> : <audio className='' controls loop autoPlay>
                 <source src={relojSrc} type="audio/mp3" />
                 <source src={relojSrc} type="audio/mp3" />
             </audio>
--- pages/paginas/relojPuente.js
+++ pages/paginas/relojPuente.js
@@ -8,7 +8,6 @@ export default function RelojPuente() {
     const [winning, seTwinning] = useState(false)
     const [eltiempo, setEltiempo] = useState(0)
     const [changing, seTchanging] = useState(false)
-    const [relojSrc, setrelojSrc] = useState('https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fy2mate.com%20-%20Reloj%20Minutero%20%20Efecto%20de%20Sonidolento.mp3?alt=media&token=aa3d406f-6724-4f87-9715-475f865f282e')
 
     useEffect(() => {
 
@@ -19,10 +18,8 @@ export default function RelojPuente() {
             switch (actionTodo) {
                 case 'llegoPlayer':                
                     seTwinning(true)
-                    setrelojSrc('https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Ffanfare-triumphal.mp3?alt=media&token=86ec38ea-bf88-4656-96aa-148b6ebd7812')
                     break;
                     case 'finalMinute':    
-                    setrelojSrc("https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Fy2mate.com%20-%20sonido%20de%20reloj%20tic%20tic.mp3?alt=media&token=b870b388-39e0-4cd7-9e52-cac388629217")
                     break;
                 case 'IniciarReloj':
                     seTchanging(true)
@@ -47,7 +44,7 @@ export default function RelojPuente() {
                 <span>  OFF</span>
             </div>
             </> : <>
-                <RelojApp relojSrc={relojSrc} winning={winning} eltiempo={eltiempo} />
+                <RelojApp  winning={winning} eltiempo={eltiempo} />
                
             </>
         }
