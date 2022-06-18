b20ad68e1b566a39c46d9408cf763a3a54c8e483
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-17 20:18:17 -0500

    moet calamarulesdassaassasdccsaca

--- pages/paginas/componenteLista.jsx
+++ pages/paginas/componenteLista.jsx
@@ -1,6 +1,6 @@
 const ComponenteLista = ({ participants = [], participantsturn = -1 }) => {
 
-    return (<div className='flex-row column Ia-flex-start bgcolor-black'>
+    return (<div className='flex-row column Ia-flex-start bgcolor-black componente-lista'>
         {participants.map((key, i) => {
             return <li className="flex-row Ia-center li-listaReady" key={`participante-${i}`}><span className={participantsturn === i ? 'li-listaReady fontcolorInedit-green':"li-listaReady"}> {i + 1}</span><span className={participantsturn === i ? 'li-listaReady fontcolorInedit-green':"li-listaReady"}>{`➟`}</span> <span className={participantsturn === i ? 'li-listaReady fontcolorInedit-green':"li-listaReady"}>{key.user}</span>{participantsturn === i ? <span className='isturn fontcolorInedit-green' >vas</span> : <></>} </li>
         })}
