b3e428807789f52d411b9e136c869eea30384110
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-17 20:11:14 -0500

    moet calamarulesdassaassasdccsaca

--- pages/paginas/listaDefinitiva.js
+++ pages/paginas/listaDefinitiva.js
@@ -39,7 +39,10 @@ export default function IstaDefinitiva() {
             'dataIn': {
                 'actionTodo': 'ipSend',
             },
+            'actionTodo': 'ipSend',
+
         })
+
     }, [])
     /*   useEffect(() => {
       
