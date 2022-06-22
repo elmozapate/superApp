93880889f6be3f41739b4c9d3829aa38d6db5908
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-06-21 19:40:53 -0500

    moet calamarturnpuenteda

--- components/containers/relojApp.jsx
+++ components/containers/relojApp.jsx
@@ -34,7 +34,7 @@ const RelojApp = ({ jail = false, winning = false, eltiempo = 0 }) => {
         <div className='reloj-puente '>
             {winning ? <div className="complete">
                 <span >PASS </span>
-                    <span className="passtime">{ '-'}{parseInt(eltiempo / 60) < 10 ? `0${parseInt(eltiempo / 60)}` : parseInt(eltiempo / 60)}:{((eltiempo) - ((parseInt(eltiempo / 60)) * 60)) < 10 ? `0${((eltiempo) - ((parseInt(eltiempo / 60)) * 60))}` : ((eltiempo) - ((parseInt(eltiempo / 60)) * 60))} </span>
+                    <span className="passtime">{ '_'}{parseInt(eltiempo / 60) < 10 ? `0${parseInt(eltiempo / 60)}` : parseInt(eltiempo / 60)}:{((eltiempo) - ((parseInt(eltiempo / 60)) * 60)) < 10 ? `0${((eltiempo) - ((parseInt(eltiempo / 60)) * 60))}` : ((eltiempo) - ((parseInt(eltiempo / 60)) * 60))} </span>
                     <audio className='hide' controls autoPlay>
                     <source src={'https://firebasestorage.googleapis.com/v0/b/avatarupload-5ed8b.appspot.com/o/calamaraudios%2Ffanfare-triumphal.mp3?alt=media&token=86ec38ea-bf88-4656-96aa-148b6ebd7812'
                     } type="audio/mp3" />
