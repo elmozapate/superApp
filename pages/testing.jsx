727d2ca7920ff333cea8d641f044879a66d3facd
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-08-14 01:13:38 -0500

    calcu
    s

--- pages/testing.jsx
+++ pages/testing.jsx
@@ -92,8 +92,20 @@ const Test = () => {
     const [resultado, setResultado] = useState({
         cosa1: 0,
         cosa2: 0,
-        cosa5: 0
+        cosa3: '+',
+        cosa5: 0,
+
     })
+    let objD = {
+        a: 'a', b: 'b', c: 'c', d: 'd'
+    }
+    let objD2 = {
+        a: 'a', b: 'b', c: 'c', d: 'd'
+    }
+    let { cosa1 = 0,
+        cosa2 = 0,
+        cosa5 = 0 } = resultado
+
     const handleLogin = (e) => {
         e.preventDefault()
         const id = e.target.id;
@@ -101,43 +113,103 @@ const Test = () => {
         console.log('handle', id, value);
         setResultado({
             ...resultado,
-            [id]: value
+            [id]: parseFloat(value)
         })
-    }
-    const operar = (value1) => {
-        const res = TestFunt(value1.cosa1, value1.cosa2)
-        console.log(res);
-        setResultado({
+        const autoOperate = {
             ...resultado,
-            cosa5: res
+            [id]: parseFloat(value)
+        }
+        operar(autoOperate)
+    }
+
+    const operar = (value, callBack = (values) => {
+        console.log(values || value, 'callBack');
+    }) => {
+        callBack()
+        callBack(TestFunt(value))
+        setResultado({
+            ...value,
+            ...TestFunt(value)
         })
     }
+    useEffect(() => {
+        const devolver = () => {
+            return {
+                a: 'a1', b: 'b2', c: 'c3'
+            }
+        }
+        const ress = devolver()
+        objD = {
+            ...objD,
+            ...ress
+        }
+        console.log(objD, 'objD2', ress);
+    }, [])
 
     return (
         <>
             <div className="IDiv-main column bgcolor-purple">
 
-                testingPage
+                AutoCalculadora
                 <br />
 
-                {resultado.cosa1}
-                <br />
-                {resultado.cosa2}
-
-                <br />
                 <input id="cosa1" value={resultado.cosa1} onChange={handleLogin} type={'number'} />
                 <br />
                 <input id="cosa2" value={resultado.cosa2} onChange={handleLogin} type={'number'} />
                 <br />
-                {resultado.cosa5}
+                <h3>{`${resultado.cosa1} ${resultado.cosa3} ${resultado.cosa2}`} </h3>
+
+
+                <br />
+                <h1> {resultado.cosa5 === 'NaN'  ? '∞' : resultado.cosa5}
+                </h1>
 
                 <br />
 
                 <button
-                    onClick={(e) => { e.preventDefault(); operar(resultado) }}
+                    className={resultado.cosa3 === "+" ? 'bgcolor-green' : 'bgcolor-red'}
+                    onClick={(e) => {
+                        e.preventDefault(); setResultado(() => {
+                            return { ...resultado, ...operar({ ...resultado, cosa3: '+' }), cosa3: '+' }
+                        })
+                    }}
                 >
-                    OPERACION
+                    SUMA
                 </button>
+                <br />
+                <button
+                    className={resultado.cosa3 === "-" ? 'bgcolor-green' : 'bgcolor-red'}
+                    onClick={(e) => {
+                        e.preventDefault(); setResultado(() => {
+                            return { ...resultado, ...operar({ ...resultado, cosa3: '-' }), cosa3: '-' }
+                        })
+                    }}
+                >
+                    RESTA
+                </button>
+                <br />
+                <button
+                    className={resultado.cosa3 === "*" ? 'bgcolor-green' : 'bgcolor-red'}
+                    onClick={(e) => {
+                        e.preventDefault(); setResultado(() => {
+                            return { ...resultado, ...operar({ ...resultado, cosa3: '*' }), cosa3: '*' }
+                        })
+                    }}
+                >
+                    MULTIPLICACION
+                </button>
+                <br />
+                <button
+                    className={resultado.cosa3 === "/" ? 'bgcolor-green' : 'bgcolor-red'}
+                    onClick={(e) => {
+                        e.preventDefault(); setResultado(() => {
+                            return { ...resultado, ...operar({ ...resultado, cosa3: '/' }), cosa3: '/' }
+                        })
+                    }}
+                >
+                    DIVISION
+                </button>
+                <br />
 
                 {/*                 <IComponent contains={objContent} />
  */}
--- pages/testingFuntion.jsx
+++ pages/testingFuntion.jsx
@@ -1,8 +1,11 @@
-export const TestFunt = (valor1, valor2) => {
+export const TestFunt = (valor) => {
 
 
-    let res = (parseInt(valor1) + parseInt(valor2))
+    let res = (valor.cosa3 === '+' ? (valor.cosa1) + (valor.cosa2) : valor.cosa3 === '-' ? (valor.cosa1) - (valor.cosa2) : valor.cosa3 === '*' ? (valor.cosa1) * (valor.cosa2) : (valor.cosa1) / (valor.cosa2))
     console.log(res, 'dentro');
-    return res
+    if (valor.cosa1 === 0 && valor.cosa2 === 0 && valor.cosa3 === '/') {
+        return { cosa5: '0 para los normales, los matematicos te pegarían' }
+    }
+    return { cosa5: res }
 }
 export default TestFunt
