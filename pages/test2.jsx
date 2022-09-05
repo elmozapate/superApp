fca8cc73ad0eab30049e6d06f7f10f316f458091
elmozapate <85320923+elmozapate@users.noreply.github.com>
2022-09-04 23:37:03 -0500

    doo

--- pages/test2.jsx
+++ pages/test2.jsx
@@ -13,7 +13,7 @@ let off = true, yaWey = true, colisioned = {
     state: false,
     item: 0,
     result: 'live'
-}, obst = [], lazyImg = [], dolor = true, sierra = true, timeOfgame = 0, actualFloor = 150, lastDireccion = 'xf', armas = {
+}, obst = [], lazyImg = [],onHitSound=true, dolor = true, sierra = true, timeOfgame = 0, actualFloor = 150, lastDireccion = 'xf', armas = {
     bat: {
         onHit: false,
         damage: 7,
@@ -569,6 +569,8 @@ const Test2 = () => {
                                         if (!armas.bat.onHit) {
                                             dibujarMalos.new[chokePlayer.array[0].b.pos].health = dibujarMalos.new[chokePlayer.array[0].b.pos].health - (armas.bat.damage * parseInt(Math.random() * 3) + 1)
                                             dibujarMalos.new[chokePlayer.array[0].b.pos].lazy = { state: true, counter: 0 }
+                                            onHitSound.play()
+
                                             joshisound2.play()
                                             if (dibujarMalos.new[chokePlayer.array[0].b.pos].health < 0) {
                                                 pow.play()
@@ -784,6 +786,7 @@ const Test2 = () => {
                                     dibujarMalos.new[i].posY = dibujarMalos.new[i].posY + dibujarMalos.new[i].heightY > (actualFloor) ? (actualFloor - dibujarMalos.new[i].heightY) : (key.state === 'onDie' || key.state === 'spirit') ? dibujarMalos.new[i].posY : !dibujarMalos.new[i].canMove.jumps.posibility ? dibujarMalos.new[i].posY : dibujarMalos.new[i].canMove.jumps.state ? (!dibujarMalos.new[i].canMove.jumps.gravity ? dibujarMalos.new[i].posY + dibujarMalos.new[i].heightY <= (actualFloor) ? dibujarMalos.new[i].posY - (.250 + dibujarMalos.new[i].canMove.jumps.speed) : (actualFloor - dibujarMalos.new[i].heightY) : dibujarMalos.new[i].posY + .250) : dibujarMalos.new[i].posY
                                     dibujarMalos.new[i].posX = (key.state === 'onDie' || key.state === 'spirit') ? dibujarMalos.new[i].posX : !dibujarMalos.new[i].canMove.walks.posibility ? dibujarMalos.new[i].posX : dibujarMalos.new[i].actions.shot.state || dibujarMalos.new[i].canMove.jumps.state ? dibujarMalos.new[i].posX : dibujarMalos.new[i].canMove.direccion === 'xf' ? dibujarMalos.new[i].posX + ((dibujarMalos.new[i].lazy.state ? .05 : .25) * dibujarMalos.new[i].canMove.walks.speed) : dibujarMalos.new[i].posX - ((dibujarMalos.new[i].lazy.state ? .05 : .25) * dibujarMalos.new[i].canMove.walks.speed)
                                     if (dibujarMalos.new[i].lazy.state) {
+                                        onHitSound.pause()
                                         dibujarMalos.new[i].lazy.counter < 1000 ? dibujarMalos.new[i].lazy.counter = dibujarMalos.new[i].lazy.counter + 1 : dibujarMalos.new[i].lazy = { counter: 0, state: false, fotograma: 0, layer: 0 }
                                     }
 
@@ -2027,6 +2030,7 @@ const Test2 = () => {
             sierra = new Audio('/audio/sierra.mp3');
             dolor = new Audio('/audio/dolor.mp3');
             dolor.volume = 0.15
+            onHitSound =new Audio('/audio/onHit.mp3');
             risabebe = new Audio('/audio/bebe-0.mp3');
             muertebebe = new Audio('/audio/bebe-2.mp3');
             pass = new Audio('/audio/pass.mp3');
