30559354098ab897058873c1adcc3949d0d24b88
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-01-21 00:47:49 -0500

    adj

--- pages/liquidarCredito/index.jsx
+++ pages/liquidarCredito/index.jsx
@@ -1,36 +1,46 @@
 import { useEffect, useState } from "react"
 import Liqui from "./liquid"
-
+const format = {
+    deuda: 0,
+    interes: 0,
+    actual: 0,
+    mes: -1,
+    historial: [],
+    valorAbono: 0,
+    abonoCapital: 0,
+    interesDeuda: 0,
+    simulacion: false,
+    deudaString: '0.00',
+    actualString: '0.00',
+    interesDeudaString: '0.00',
+    valorAbonoString: '0.00',
+    incurse: true,
+    historyReverse:[]
+}
 const Liquidador = (props) => {
 
     let elemto1 = true
     let elemto2 = true
-    const [deuda, setDeuda] = useState({
-        deuda: 0,
-        interes: 0,
-        actual: 0,
-        mes: -1,
-        historial: [],
-        valorAbono: 0,
-        abonoCapital: 0,
-        interesDeuda: 0,
-        simulacion: false
-    })
+    const [deuda, setDeuda] = useState(format)
 
     const crearCredito = () => {
         const newHstorial = [{
             tipo: 'Inicio Prestamo',
             actual: parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)),
             valorAbono: 0,
+            actualString: !isNaN(parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) && parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) : 0,
+            interesDeudaString: !isNaN(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) && parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) : 0,
             interesDeuda: (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)),
         }]
         setDeuda({
             ...deuda,
+            incurse: true,
             actual: parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)),
             mes: deuda.mes + 1,
             interesDeuda: (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)),
             historial: newHstorial,
-
+            interesDeudaString: !isNaN(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) && parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) : 0,
+            actualString: !isNaN(parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) && parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat(parseFloat(parseFloat(deuda.deuda)) + (parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) : 0,
         })
 
     }
@@ -39,22 +49,33 @@ const Liquidador = (props) => {
         elemto2 = document.getElementById('elemto2')
         const newDeuda = parseFloat(parseFloat(deuda.actual)) + (parseFloat(deuda.actual / 100) * parseFloat(deuda.interes))
         let newHstorial = deuda.historial
+        
         const newDAta = {
             valorAbono: 0,
             tipo: 'Mes nuevo',
             actual: newDeuda > 0 ? newDeuda : 0,
             mes: deuda.mes + 1,
             interesDeuda: (parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)),
+            interesDeudaString: !isNaN(parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)))) && parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)))) : 0,
+            actualString: !isNaN(parseFloat(newDeuda > 0 ? newDeuda : 0)) && parseFloat(newDeuda > 0 ? newDeuda : 0) >= 0 ? formatoMiles(parseFloat(newDeuda > 0 ? newDeuda : 0)) : 0,
         }
-
+        /* let newHstorialR = [newDAta]
+        let reversed=newHstorial
+        reversed.reverse()
+        reversed.map((key)=>{
+            newHstorialR.push(key)
+        }) */
         newHstorial.push(newDAta)
         setDeuda({
             ...deuda,
             actual: newDeuda > 0 ? newDeuda : 0,
+            actualString: !isNaN(parseFloat(newDeuda > 0 ? newDeuda : 0)) && parseFloat(newDeuda > 0 ? newDeuda : 0) >= 0 ? formatoMiles(parseFloat(newDeuda > 0 ? newDeuda : 0)) : 0,
             mes: deuda.mes + 1,
             interesDeuda: (parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)),
-            historial: newHstorial
-        })
+            interesDeudaString: !isNaN(parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)))) && parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat((parseFloat(deuda.actual / 100) * parseFloat(deuda.interes)))) : 0,
+            historial: newHstorial,
+/*             historyReverse:newHstorialR
+ */        })
         if (deuda.simulacion) {
 
             setTimeout(() => {
@@ -63,18 +84,38 @@ const Liquidador = (props) => {
 
                 } catch (error) {
                     console.log('listo');
+                    window.alert('Finalizado')
+                    setDeuda({
+                        ...deuda,
+                        incurse: false,
+                    })
                 }
             }, 300);
         }
+    }
+    const formatoMiles = (number) => {
+        const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+        const rep = '$1,';
+        let arr = !isNaN(number) ? parseFloat(number).toFixed(2).toString().split('.') : number.toString().split('.');
+        arr[0] = arr[0].replace(exp, rep);
+        return arr[1] ? arr.join('.') : arr[0];
     }
     const handle = (e) => {
         e.preventDefault();
         const id = e.target.id
         const value = e.target.value
-        setDeuda({
-            ...deuda,
-            [id]: !isNaN(parseFloat(value)) && parseFloat(value) >= 0 ? parseFloat(value) : 0
-        })
+        if (id === 'deuda' || id === 'actual' || id === 'interesDeuda' || id === 'valorAbono') {
+            setDeuda({
+                ...deuda,
+                [id]: !isNaN(parseFloat(value)) && parseFloat(value) >= 0 ? parseFloat(value) : 0,
+                [`${id}String`]: !isNaN(parseFloat(value)) && parseFloat(value) >= 0 ? formatoMiles(parseFloat(value)) : 0
+            })
+        } else {
+            setDeuda({
+                ...deuda,
+                [id]: !isNaN(parseFloat(value)) && parseFloat(value) >= 0 ? parseFloat(value) : 0,
+            })
+        }
     }
     const abonoCapital = () => {
         let newHstorial = deuda.historial
@@ -86,13 +127,23 @@ const Liquidador = (props) => {
             actual: parseFloat(deuda.actual) - deuda.valorAbono,
             interesDeuda: (deuda.interesDeuda),
             valorAbono: (deuda.valorAbono),
+            actualString: !isNaN(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) && parseFloat(parseFloat(deuda.actual - deuda.valorAbono)) >= 0 ? formatoMiles(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) : 0,
+            interesDeudaString: !isNaN(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) && parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes))) >= 0 ? formatoMiles(parseFloat((parseFloat(deuda.deuda / 100) * parseFloat(deuda.interes)))) : 0,
+            valorAbonoString: formatoMiles(deuda.valorAbono)
         }
+     /*    let newHstorialR = [newDAta]
+        let reversed=newHstorial
+        reversed.reverse()
+        reversed.map((key)=>{
+            newHstorialR.push(key)
+        }) */
         newHstorial.push(newDAta)
-
         setDeuda({
             ...deuda,
             actual: parseFloat(deuda.actual - deuda.valorAbono),
-            valorAbono: deuda.simulacion ? deuda.valorAbono : 0
+            actualString: !isNaN(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) && parseFloat(parseFloat(deuda.actual - deuda.valorAbono)) >= 0 ? formatoMiles(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) : 0,
+            valorAbono: deuda.simulacion ? deuda.valorAbono : 0,
+
         })
         if (deuda.simulacion) {
             setTimeout(() => {
@@ -101,6 +152,11 @@ const Liquidador = (props) => {
 
                 } catch (error) {
                     console.log('listo');
+                    window.alert('Finalizado')
+                    setDeuda({
+                        ...deuda,
+                        incurse: false,
+                    })
                 }
             }, 300);
         }
--- pages/liquidarCredito/liquid.jsx
+++ pages/liquidarCredito/liquid.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 const Liqui = (props) => {
     const { deuda = {
         deuda: 0,
@@ -8,30 +10,45 @@ const Liqui = (props) => {
         valorAbono: 0,
         abonoCapital: 0,
         interesDeuda: 0,
-        simulacion: false
+        simulacion: false,
+        deudaString: '0.00',
+        actualString: '0.00',
+        interesDeudaString: '0.00',
+        valorAbonoString: '0.00',
+        incurse: true,
+        historyReverse:[]
 
     }, simulacion = console.log, setDeuda = console.log, mesSiguente = console.log, abonoCapital = console.log, handle = console.log, crearCredito = console.log } = props
+    const [table, setTable] = useState({
+        array: []
+    })
+ /*    useEffect(() => {
+        let comp = deuda.historial.reverse()
+        if (comp !== table.array && false === true) {
+            setTable({ ...table, array: deuda.historial.reverse() })
+        }
+    }, [deuda]) */
     return (
         <div className="fill ">
             <div className="rowL">
-            <div className="liquidar">
-                    <h1>Deuda : {`$ ${deuda.deuda.toFixed(2)}`}</h1>
-                    <h1>Deuda Actual : {`$ ${deuda.actual.toFixed(2)}`}</h1>
+                <div className="liquidar">
+                    <h1>Deuda : {`$ ${deuda.deudaString}`}</h1>
+                    <h1>Deuda Actual : {`$ ${deuda.actualString}`}</h1>
                     <h2>Interes prestamo : {(deuda.interes)} %</h2>
                     <h2>Año- {deuda.mes / 12 >= 1 ? parseInt(deuda.mes / 12) : 0}</h2>
 
                     <h2>Mes- {deuda.mes - (parseInt(deuda.mes / 12) * 12) + 1}</h2>
-                    <h2> Interes Deuda : {`$ ${deuda.interesDeuda.toFixed(2)}`} </h2>
-                    <h3>Valor abono : {`$ ${deuda.valorAbono.toFixed(2)}`}</h3>
+                    <h2> Interes Deuda : {`$ ${deuda.interesDeudaString}`} </h2>
+                    <h3>Valor abono : {`$ ${deuda.valorAbonoString}`}</h3>
                     {deuda.mes === -1 && <>DEUDA INICIAL<input type={'number'} placeHolder={'deuda'} value={deuda.deuda} onChange={handle} id={'deuda'} /></>}
                     <br />
                     {deuda.mes === -1 && <>PORCENTAJE DE INTERES<input type={'number'} placeHolder={'Interes prestamo'} value={deuda.interes} onChange={handle} id={'interes'} /></>}
-                    {deuda.mes > -1 && <>VALOR DEL ABOMO<input type={'number'} value={deuda.valorAbono} onChange={handle} id={'valorAbono'} min={deuda.interesDeuda} defaultValue={deuda.interesDeuda} /></>}
+                    {deuda.mes > -1 && <>VALOR DEL ABONO<input type={'number'} value={deuda.valorAbono} onChange={handle} id={'valorAbono'} min={deuda.interesDeuda} defaultValue={deuda.interesDeuda} /></>}
                     {deuda.actual > 0 && deuda.mes > -1 && <button id={'elemto2'} onClick={(e) => { e.preventDefault(); abonoCapital() }}>ABONO</button>}
                     {deuda.actual > 0 && deuda.mes > -1 && <button onClick={(e) => {
                         e.preventDefault(); simulacion()
                     }}>{deuda.simulacion ? 'Detener Simulacion' : 'Simulacion'}</button>}
-                    <button id={'elemto1'} onClick={(e) => { e.preventDefault(); deuda.actual !== 0 && deuda.mes > -1 ? mesSiguente() : crearCredito() }}>{deuda.mes > -1 ? 'SIGUENTE MES' : 'CREAR PRESTAMO'}</button>
+                    {deuda.incurse && <button id={'elemto1'} onClick={(e) => { e.preventDefault(); deuda.actual !== 0 && deuda.mes > -1 ? mesSiguente() : crearCredito() }}>{deuda.mes > -1 ? 'SIGUENTE MES' : 'CREAR PRESTAMO'}</button>}
                     <button onClick={(e) => {
                         e.preventDefault(); setDeuda({
                             deuda: 0,
@@ -42,7 +59,12 @@ const Liqui = (props) => {
                             valorAbono: 0,
                             abonoCapital: 0,
                             interesDeuda: 0,
-
+                            deudaString: '0.00',
+                            actualString: '0.00',
+                            interesDeudaString: '0.00',
+                            valorAbonoString: '0.00',
+                            incurse: true,
+                            historyReverse:[]
                         })
                     }}>REINICIAR</button>
 
@@ -62,9 +84,9 @@ const Liqui = (props) => {
                                 <tr id={`data1-${i}`}>
                                     <td id={`data2-${i}`}>{key.tipo}</td>
                                     <td id={`data2-${i}`}>{key.mes}</td>
-                                    <td id={`data3-${i}`}>{`$ ${key.actual.toFixed(2)}`}</td>
-                                    <td id={`data4-${i}`}>{key.interesDeuda}</td>
-                                    <td id={`data4-${i}`}> {key.tipo === 'Abono' ? `$ ${key.valorAbono.toFixed(2)}` : 'no aplica'}</td>
+                                    <td id={`data3-${i}`}>{`$ ${key.actualString}`}</td>
+                                    <td id={`data4-${i}`}>{`$ ${key.interesDeudaString}`}</td>
+                                    <td id={`data4-${i}`}> {key.tipo === 'Abono' ? `$ ${key.valorAbonoString}` : 'no aplica'}</td>
 
                                 </tr>
                             </>
