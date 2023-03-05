36dc98c945fb07a17824dc7572ed981d0f19bc3a
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-03-05 10:34:37 -0500

    Update index.jsx

--- pages/liquidarCredito/index.jsx
+++ pages/liquidarCredito/index.jsx
@@ -157,7 +157,7 @@ const Liquidador = (props) => {
             ...deuda,
             actual: parseFloat(deuda.actual - deuda.valorAbono),
             actualString: !isNaN(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) && parseFloat(parseFloat(deuda.actual - deuda.valorAbono)) >= 0 ? formatoMiles(parseFloat(parseFloat(deuda.actual - deuda.valorAbono))) : 0,
-            valorAbono: deuda.simulacion ? parseInt((MesesDeuda[parseInt(deuda.mes / 12)][(deuda.mes - (parseInt(deuda.mes / 12) * 12))]) !== 1) ? MesesDeuda[parseInt(deuda.mes / 12)][(deuda.mes - (parseInt(deuda.mes / 12) * 12))] : parseInt(deuda.interesDeudaString.replace(',','')) : 0,
+            valorAbono: deuda.simulacion ? parseInt(MesesDeuda[parseInt(deuda.mes / 12)][(deuda.mes - (parseInt(deuda.mes / 12) * 12))])!== 1 ? MesesDeuda[parseInt(deuda.mes / 12)][(deuda.mes - (parseInt(deuda.mes / 12) * 12))] : parseInt(deuda.interesDeudaString.replace(',','')) : 0,
             historial: newHstorial
         })
         const deudaConst = deudaAcumuled
