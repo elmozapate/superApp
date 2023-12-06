cf5a8ab79099af2163c9567dab730c41670d42cc
elmozapate <85320923+elmozapate@users.noreply.github.com>
2023-12-06 11:40:34 -0500

    Update envMachetero.jsx

--- envMachetero.jsx
+++ envMachetero.jsx
@@ -6,9 +6,9 @@ export const EnvM = () => {
     3 => Devlocal
     4 => DevDev
      */
-    const mode=1  /* COLOCAR EL NUMERO DEL MODO */
+    const mode=4  /* COLOCAR EL NUMERO DEL MODO */
     const ambiente = mode === 1 ? 'locallocal' : mode === 2 ? 'localDev' : mode === 3 ? 'Devlocal' : mode === 4 ? 'DevDev' : 'bad'
-    const HostBack = ambiente === 'locallocal' ? 'http://localhost:3002/' : ambiente === 'localDev' ? "https://serverazteca.herokuapp.com/" : ambiente === 'Devlocal' ? 'http://localhost:3002/' : ambiente === 'DevDev' ? "https://serverazteca.herokuapp.com/" : 'bad'
+    const HostBack = ambiente === 'locallocal' ? 'http://localhost:3002/' : ambiente === 'localDev' ? "https://full-bingo.onrender.com/" : ambiente === 'Devlocal' ? 'http://localhost:3002/' : ambiente === 'DevDev' ? "https://full-bingo.onrender.com/" : 'bad'
     const HostBackEnd = ambiente === 'locallocal' ? 'http://localhost:3001' : ambiente === 'localDev' ? "https://serverproto.herokuapp.com" : ambiente === 'Devlocal' ? 'http://localhost:3001' : ambiente === 'DevDev' ? "https://serverproto.herokuapp.com" : 'bad'
     const HostFront = ambiente === 'locallocal' ? 'http://localhost:3000/' : ambiente === 'localDev' ? 'http://localhost:3000/' : ambiente === 'Devlocal' ? 'https://super-app-liard.vercel.app/' : ambiente === 'DevDev' ? "https://super-app-liard.vercel.app/" : 'bad'
     const envMachete = {
